76c76c25ec7b143a5e1a6c31cd6f904e146bebbd
LaroSelf <laroself1@gmail.com>
2016-11-13 20:31:20 +0200

    new parameters for calendar

---
+++ test/Tests.java
@@ -0,0 +1,20 @@
+/**
+ * Created by LaroSelf on 13.11.2016.
+ */
+
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class Tests {
+
+    @Test
+    public void checkCorrectParsing(){
+
+        String httpRequest = "http://greater.local:9999/calendar/?firstday=SATURDAY&year=2024&month=APRIL";
+    }
+}
