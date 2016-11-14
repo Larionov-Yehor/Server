888384e1be6c9d64360088be587d86b386158dd1
LaroSelf <laroself1@gmail.com>
2016-11-14 07:33:06 +0200

    tests

--- test/Tests.java
+++ test/Tests.java
@@ -3,7 +3,21 @@
  */
 
 
+import org.junit.Before;
 import org.junit.Test;
+import server.HttpRequest;
+import server.Parser;
+import server.implementation.ParserImpl;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
@@ -12,9 +26,32 @@ import static org.hamcrest.Matchers.is;
 
 public class Tests {
 
-    @Test
-    public void checkCorrectParsing(){
+    private Parser parser = new ParserImpl();
+    private HttpRequest httpRequest;
 
-        String httpRequest = "http://greater.local:9999/calendar/?firstday=SATURDAY&year=2024&month=APRIL";
+
+
+    @Test
+    public void assertCorrectParametersGet() throws IOException{
+        String request = "GET /calendar/?firstday=WEDNESDAY&year=1994&month=MAY";
+        InputStream inputStream = new ByteArrayInputStream(request.getBytes(StandardCharsets.UTF_8));
+        httpRequest = parser.parse(inputStream);
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("firstday", "WEDNESDAY");
+        parameters.put("year", "1994");
+        parameters.put("month", "MAY");
+        assertThat(parameters.get("firstday"), equalTo(httpRequest.getParameters().get("firstday")));
+        assertThat(parameters.get("year"), equalTo(httpRequest.getParameters().get("year")));
+        assertThat(parameters.get("month"), equalTo(httpRequest.getParameters().get("month")));
+
     }
+    @Test
+    public void assertCorrectPathGet() throws IOException {
+        String rawRequest = "GET /greeter/?name=Alex";
+        httpRequest = parser.parse(new ByteArrayInputStream(rawRequest.getBytes(StandardCharsets.UTF_8)));
+
+        assertThat("/greeter", equalTo(httpRequest.getPath()));
+
+    }
+
 }
